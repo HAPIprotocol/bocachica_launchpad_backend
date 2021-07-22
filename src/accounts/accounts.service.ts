e0da4a1b658196849f67032be8f5044023553dae
Andrei Arsenin <andrei.arsenin@hapi.one>
2021-07-22 10:57:55 +0300

    Added account creation on balance request

--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -1,13 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { LAMPORTS_PER_SOL } from '@solana/web3.js';
-import BN from 'bn.js';
+import * as BN from 'bn.js';
 
 import { UserAccount } from './entities/user-account.entity';
 
 @Injectable()
 export class AccountsService {
+  private readonly logger = new Logger(AccountsService.name);
+
   constructor(
     @InjectRepository(UserAccount)
     private readonly userAccountRepo: Repository<UserAccount>,
@@ -15,10 +16,24 @@ export class AccountsService {
 
   async updateBalance(address: string, lamports: number): Promise<boolean> {
     try {
-      const balance = new BN(lamports).div(new BN(LAMPORTS_PER_SOL)).toString();
-      await this.userAccountRepo.update({ publicKey: address }, { balance });
+      const balance = new BN(lamports).toString();
+      let userAccount = await this.userAccountRepo.findOne({
+        publicKey: address,
+      });
+      if (userAccount) {
+        await this.userAccountRepo.update({ publicKey: address }, { balance });
+      } else {
+        userAccount = new UserAccount();
+        userAccount.publicKey = address;
+        userAccount.balance = balance;
+        await this.userAccountRepo.save(userAccount);
+        this.logger.verbose(
+          `Account with address ${address} has been registered`,
+        );
+      }
       return true;
-    } catch (_) {
+    } catch (error) {
+      this.logger.error(error.message);
       return false;
     }
   }
