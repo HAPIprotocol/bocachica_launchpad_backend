95c078877154458b5937675392054f4d046dc85b
Andrei Arsenin <andrei.arsenin@hapi.one>
2021-07-27 16:08:52 +0300

    Added hex signature test

--- src/tickets/tickets.service.spec.ts
+++ src/tickets/tickets.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import * as bs58 from 'bs58';
 
 import {
   IMockEntityProvider,
@@ -25,12 +26,27 @@ describe('TicketsService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should verify signature', async () => {
+  it('should verify signature 1', async () => {
     const publicKey = 'Cs4fUYKWy5ZHew7qAJgoev6qrpEErF4yw5zZmKoQmw75';
     const signature =
       '4Ts737ZTqsAbSK9KHKuF1sB31cQFjBCDLLRoii5ZupTzx71iLen1QuETFvt5fkF2f1yBHNHBoDRaYbJ9B6Rbwem7';
     const message = 'Ere we go, ere we go, cross the Kosmos.';
 
+    const ticket = new Ticket();
+    Object.assign(ticket, { publicKey, signature, message } as Partial<Ticket>);
+
+    expect(service.verifyTicketSignature(ticket)).toBeTruthy();
+  });
+
+  it('should verify signature 2', async () => {
+    const publicKey = '3CEXr4pyPo2DUbnCNnt8ZjfVq6b4tvrnSx6q1omRxGGW';
+    const signature = bs58.encode(
+      Buffer.from(
+        '751613f040dd8135258e0e7be66dfcdfaf55a1065c914a0e235f1cf73ee927f30c0c91a388053220fa2fe521bfa974eb97115472d5c0215fbc5cd048b4a6db0c',
+        'hex',
+      ),
+    );
+    const message = 'I want to join project #1 on Boca Chica!';
     const ticket = new Ticket();
     Object.assign(ticket, { publicKey, signature, message } as Partial<Ticket>);
 
