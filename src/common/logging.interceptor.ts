c35fed32bdcc35bd8640936f4179cf185abbc5d1
Andrei Arsenin <andrei.arsenin@hapi.one>
2021-08-18 12:37:15 +0300

    Enhanced request logging

--- src/common/logging.interceptor.ts
+++ src/common/logging.interceptor.ts
@@ -7,28 +7,52 @@ import {
   InternalServerErrorException,
   Logger,
 } from '@nestjs/common';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 import { Observable, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import { flobj } from './string';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-  private logger = new Logger('Request');
+  private logger = new Logger('HTTP');
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const request: Request = context.switchToHttp().getRequest();
     const reqId = Math.floor(Math.random() * 2821109907456).toString(36);
-    const reqLog = `[${reqId}] ${request.method} ${request.path} ip=${request.ip}`;
-    this.logger.debug(reqLog);
+    const ip =
+      request.header('X-Real-IP') ||
+      request.header('X-Forwarded-For') ||
+      request.ip;
+
+    this.logger.verbose(
+      `[Request:${reqId}] ${flobj({
+        method: request.method,
+        path: request.path,
+        ip,
+      })}`,
+    );
 
     const now = Date.now();
     return next.handle().pipe(
       tap(() => {
-        this.logger.log(`${reqLog} time=${Date.now() - now}ms`);
+        const response = context.switchToHttp().getResponse<Response>();
+        this.logger.verbose(
+          `[Response:${reqId}] ${flobj({
+            time: Date.now() - now,
+            code: response.statusCode,
+          })}`,
+        );
       }),
       catchError((err) =>
         throwError(() => {
-          this.logger.error(`${reqLog} ${err.message}`);
+          const response = context.switchToHttp().getResponse<Response>();
+          this.logger.error(
+            `[Response:${reqId}] ${flobj({
+              error: err.message,
+              time: Date.now() - now,
+              code: response.statusCode,
+            })}`,
+          );
           return err instanceof HttpException
             ? err
             : new InternalServerErrorException();
